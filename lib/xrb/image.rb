7e02c8b08eac1fc9521579251cdc780be587e95d
dan sinclair <dj2@everburning.com>
2012-06-16 20:25:53 -0700

    convert drawable to id

--- lib/xrb/image.rb
+++ lib/xrb/image.rb
@@ -2,6 +2,9 @@ module Xrb
   class Image
     def initialize(conn, opts = {})
       @conn = conn
+
+      opts[:drawable] = opts[:drawable].id
+
       @conn.send(Xrb::Request::GetImage.new(opts))
     end
   end
