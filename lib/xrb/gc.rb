960cb6b77fd6b0d3b5bafe07fb4360eac095cff2
dan sinclair <dj2@everburning.com>
2012-06-18 10:13:33 -0700

    gc methods

--- lib/xrb/gc.rb
+++ lib/xrb/gc.rb
@@ -16,5 +16,29 @@ module Xrb
 
       @conn.send(Xrb::Request::CreateGc.new(o))
     end
+
+    def free
+      @conn.send(Xrb::Request::FreeGC.new(gc: @id))
+    end
+
+    def change(opts)
+      o = opts.merge(gc: @id)
+      @conn.send(Xrb::Request::ChangeGC.new(o))
+    end
+
+    def copy(opts)
+      o = opts.merge(src_gc: @id)
+      @conn.send(Xrb::Request::CopyGC.new(o))
+    end
+
+    def set_dashes(opts)
+      o = opts.merge(gc: @id)
+      @conn.send(Xrb::Request::SetDashes.new(o))
+    end
+
+    def set_clip_rectangles(opts)
+      o = opts.merge(gc: @id)
+      @conn.send(Xrb::Request::SetClipRectangles.new(o))
+    end
   end
 end
