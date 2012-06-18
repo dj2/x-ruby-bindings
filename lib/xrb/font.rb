30452eb04a77ea5c3d45b0d95bdf1e86cb4a73f8
dan sinclair <dj2@everburning.com>
2012-06-18 10:09:03 -0700

    add font methods

--- lib/xrb/font.rb
+++ lib/xrb/font.rb
@@ -11,5 +11,43 @@ module Xrb
       o = {fid: @id, name_len: @name.length, name: @name}
       @conn.send(Xrb::Request::OpenFont.new(o))
     end
+
+    def close
+      @conn.send(Xrb::Request::CloseFont.new(font: @id))
+    end
+
+    def query(fontable, &blk)
+      cookie = @conn.send(Xrb::Request::QueryFont.new(fontable: fontable))
+      cookie.callback = blk if block_given?
+      cookie
+    end
+
+    def query_text_extends(opts)
+      cookie = @conn.send(Xrb::Request::QueryTextExtends.new(opts))
+      cookie.callback = blk if block_given?
+      cookie
+    end
+
+    def list(opts, &blk)
+      cookie = @conn.send(Xrb::Request::ListFonts.new(opts))
+      cookie.callback = blk if block_given?
+      cookie
+    end
+
+    def list_with_info(opts, &blk)
+      cookie = @conn.send(Xrb::Request::ListFontWithInfo.new(opts))
+      cookie.callback = blk if block_given?
+      cookie
+    end
+
+    def set_font_paths(paths)
+      @conn.send(Xrb::Request::SetFontPath.new(path: paths))
+    end
+
+    def font_path(&blk)
+      cookie = @conn.send(Xrb::Request::GetFontPath.new)
+      cookie.callback = blk if block_given?
+      cookie
+    end
   end
 end
